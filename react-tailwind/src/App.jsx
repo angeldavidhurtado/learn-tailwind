ad5476064f45db241988929786c494e18e758098
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-08-21 18:14:46 -0500

    test react tailwind

--- react-tailwind/src/App.jsx
+++ react-tailwind/src/App.jsx
@@ -1,7 +1,16 @@
 function App() {
   return (
     <>
-      <h1 className='bg-blue'>Vite + React</h1>
+      <h1 className='bg-coffe text-[#FFEECF]'>
+        Vite + React
+      </h1>
+      <p className='w-1/4 bg-amber-800 text-amber-100 m-auto'>
+        <span className='text-blue-500'>hola</span> mundo
+      </p>
+
+      <div className='size-28 bg-teal-300 w-'></div>
+      <div className='size-card bg-violet-600 rounded-md'></div>
+      <div className='w-32 h-24 border-4 border-orange-400 bg-zinc-400 rounded-md'></div>
     </>
   )
 }
