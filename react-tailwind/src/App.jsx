938ba768a6410456c167fac46a82d28b3663d0c5
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-08-22 12:59:18 -0500

    responsive

--- react-tailwind/src/App.jsx
+++ react-tailwind/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className='bg-coffe text-[#FFEECF]'>
+      <h1 className='bg-coffe text-[#FFEECF] sm:bg-cyan-800'>
         Vite + React
       </h1>
       <p className='w-1/4 bg-amber-800 text-amber-100 m-auto'>
@@ -13,10 +13,10 @@ function App() {
       <div className='w-32 h-24 border-4 border-orange-400 bg-zinc-400 rounded-md'></div>
 
       <div className='grid grid-cols-3 text-white border border-amber-200 w-3xs m-5 gap-2 rounded-sm'>
-        <div className='bg-violet-800 text-center rounded-sm'>1</div>
-        <div className='bg-violet-800 text-center rounded-sm'>2</div>
+        <div className='bg-violet-800 text-center rounded-sm col-start-2'>1</div>
+        <div className='bg-violet-800 text-center rounded-sm col-start-1 col-span-full'>2</div>
         <div className='bg-violet-800 text-center rounded-sm'>3</div>
-        <div className='bg-violet-800 text-center rounded-sm'>4</div>
+        <div className='bg-violet-800 text-center rounded-sm -col-end-1 col-span-2'>4</div>
       </div>
     </>
   )
