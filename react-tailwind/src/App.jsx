8407d3ea9f473091ad08251d5d305ffd7332769b
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-08-22 11:30:27 -0500

    grid

--- react-tailwind/src/App.jsx
+++ react-tailwind/src/App.jsx
@@ -11,6 +11,13 @@ function App() {
       <div className='size-28 bg-teal-300 w-'></div>
       <div className='size-card bg-violet-600 rounded-md'></div>
       <div className='w-32 h-24 border-4 border-orange-400 bg-zinc-400 rounded-md'></div>
+
+      <div className='grid grid-cols-3 text-white border border-amber-200 w-3xs m-5 gap-2 rounded-sm'>
+        <div className='bg-violet-800 text-center rounded-sm'>1</div>
+        <div className='bg-violet-800 text-center rounded-sm'>2</div>
+        <div className='bg-violet-800 text-center rounded-sm'>3</div>
+        <div className='bg-violet-800 text-center rounded-sm'>4</div>
+      </div>
     </>
   )
 }
