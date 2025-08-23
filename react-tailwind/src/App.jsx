b418cff7f5258faf622c8cadd80fa0bc3267dd1e
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-08-23 14:20:44 -0500

    apply

--- react-tailwind/src/App.jsx
+++ react-tailwind/src/App.jsx
@@ -26,10 +26,10 @@ function App() {
       <div className='w-32 h-24 border-4 border-orange-400 bg-zinc-400 rounded-md'></div>
 
       <div className='grid grid-cols-3 text-white border border-amber-200 w-3xs m-5 gap-2 rounded-sm'>
-        <div className='bg-violet-800 text-center rounded-sm col-start-2'>1</div>
-        <div className='bg-violet-800 text-center rounded-sm col-start-1 col-span-full'>2</div>
-        <div className='bg-violet-800 text-center rounded-sm'>3</div>
-        <div className='bg-violet-800 text-center rounded-sm -col-end-1 col-span-2'>4</div>
+        <div className='button col-start-2'>1</div>
+        <div className='button col-start-1 col-span-full'>2</div>
+        <div className='button'>3</div>
+        <div className='button -col-end-1 col-span-2'>4</div>
       </div>
 
       <p className='text-white tablet:max-desktop:bg-amber-200 tablet:max-desktop:text-black'>
@@ -44,7 +44,7 @@ function App() {
         </div>
       </div>
 
-      <button className='hover:bg-blue-800 bg-blue-700 cursor-pointer text-white text-lg px-4 py-2 rounded-md'>
+      <button className='hover:bg-blue-800 bg-blue-700 cursor-pointer text-white text-lg px-4 py-2 rounded-md transition-colors duration-300'>
         Hola
       </button>
 
