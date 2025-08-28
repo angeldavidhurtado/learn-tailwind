decbfc944c618c7e81a2e129d8c33e87215e0f4e
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-08-28 14:35:30 -0500

    add ico

--- tailwind-practice/src/App.jsx
+++ tailwind-practice/src/App.jsx
@@ -1,7 +1,19 @@
+import icons from './assets/icons.svg'
+
 function App() {
   return (
     <>
       <h1 className='text-center'>Hola mundo</h1>
+
+      <img
+        className='rounded-full'
+        src="https://angeldavidhurtado.github.io/img/angel-david-hurtado.webp"
+        alt="Ángel David Hurtado"
+      />
+
+      <svg className='fill-amber-100 size-5'>
+        <use href={`${icons}#email`} />
+      </svg>
     </>
   )
 }
