74a6174b6066d195d51468d1b87bbbb92a954a6f
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-08-28 18:15:33 -0500

    main section finished

--- tailwind-practice/src/pages/Profile.jsx
+++ tailwind-practice/src/pages/Profile.jsx
@@ -2,19 +2,33 @@ import Icon from './../components/Icon'
 
 function Profile() {
   return (
-    <>
-      <h1 className='text-center'>Hola mundo</h1>
-
+    <div className='max-w-96 m-auto'>
       <img
-        className='h-96 mask-radial-at-center mask-radial-from-45% mask-radial-at-80%'
+        className='h-96 mask-radial-at-center mask-radial-from-45% mask-radial-to-75%'
         src="https://angeldavidhurtado.github.io/img/angel-david-hurtado.webp"
         alt="Ángel David Hurtado"
       />
 
-      <Icon iconSelect="email" />
-      <Icon iconSelect="linkedin" />
-      <Icon iconSelect="email" />
-    </>
+      <h1 className='text-center text-3xl font-bold my-4'>Ángel David Hurtado</h1>
+
+      <ul className='flex gap-2 justify-center'>
+        <li>
+          <a href="https://www.linkedin.com/in/angeldavidhurtado" target='_blank'>
+            <Icon iconSelect="linkedin" />
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/angeldavidhurtado" target='_blank'>
+            <Icon iconSelect="github" />
+          </a>
+        </li>
+        <li>
+          <a href="https://medium.com/@angeldavidhurtado" target='_blank'>
+            <Icon iconSelect="medium" />
+          </a>
+        </li>
+      </ul>
+    </div>
   )
 }
 
